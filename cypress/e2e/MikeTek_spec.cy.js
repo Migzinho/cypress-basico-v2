5af904e3959e8842b35ae6b6bbe29ed739eb5f9f
Emidio Mignozzetti <emidio.mignozzetti@gmail.com>
2024-04-25 00:31:23 -0300

    Correção do spec Miketek_v2

--- cypress/e2e/MikeTek_spec.cy.js
+++ cypress/e2e/MikeTek_spec.cy.js
@@ -3,7 +3,7 @@ describe('Empresa Miketec', function() {
     it('visitar o site da Miketec', function(){
         cy.visit('https://miketec.com.br/')
     })
-   it('verifica o título da aplicação', function() {
+   it.only('verifica o título da aplicação', function() {
     cy.visit('https://miketec.com.br/')
     cy.title().should('be.equal', 'MikeTec: Tecnologia para o sucesso do seu negócio de turismo')
   })
@@ -15,12 +15,12 @@ describe('Empresa Miketec', function() {
       .should('be.equal', 'MikeTec BackOffice: O futuro da eficiência no turismo')
     })
 
-  it('verifica o Provider', function(){
+  it.only('verifica o Provider', function(){
    cy.visit('https://miketec.com.br/');
     cy.get('.et_pb_button_2')
       .click().title().should('be.equal', 'Provider - Miketec')
   })
-  it('verifica o Implementação e Treinamento', function(){
+  it.only('verifica o Implementação e Treinamento', function(){
     cy.visit('https://miketec.com.br/');
     cy.get('.et_pb_button_4')
       .click().title().should('be.equal', 'Implementação e Treinamento - Miketec')
